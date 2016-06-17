a5ccc7b3a6b6c94a83d99c0b6f91771596faf141
Martin Horský <martin.horsky@slevomat.cz>
2016-06-17 16:30:33 +0200

    Add Hele button container and attributes

--- hele-button/src/index.js
+++ hele-button/src/index.js
@@ -1,39 +1,69 @@
 import { MJMLElement } from 'mjml-core';
 import MJMLButton from 'mjml-button';
+import MJMLColumn from 'mjml-column';
+import MJMLSection from 'mjml-section';
 import React, { Component } from 'react';
+
+import cleanContent from '../../helpers/cleanContent';
 
 const tagName = 'hele-button';
 const endingTag = true;
 const columnElement = false;
 
-const defaultMJMLDefinition = { };
+const defaultMJMLDefinition = {
+	attributes: {
+		'href': '',
+		'full-width': false,
+	},
+};
 
-const baseStyles = { };
+const baseStyles = {};
 
 @MJMLElement
 class HeleButton extends Component {
-  render () {
-    const { mjContent } = this.props;
 
-    return (
-      <MJMLButton
-        background-color="#41b79d"
-        color="white"
-        border-radius="0"
-        padding="7px 5px"
-        align="middle"
-        container-background-color="#41b79d"
-        font-size="14px"
-        font-weight="600"
-        font-family="Arial, Helvetica, sans-serif"
-      >
-        <span
-          style={{ textTransform: 'uppercase' }}
-          dangerouslySetInnerHTML={{ __html: mjContent() }}
-        />
-      </MJMLButton>
-    )
-  }
+	isFullWidth () {
+		const { mjAttribute } = this.props;
+
+		return mjAttribute('full-width') !== false;
+	}
+
+	render() {
+		const { mjContent, mjAttribute } = this.props;
+
+		const content = cleanContent(mjContent());
+
+		const isFullWidth = this.isFullWidth();
+
+		return (
+			<MJMLSection
+				parentWidth={this.props.parentWidth}
+				padding="10px 10px"
+				text-align={isFullWidth ? 'center' : 'left'}
+			>
+				<MJMLColumn
+					width={isFullWidth ? '100%' : '300px'}
+				>
+					<MJMLButton
+						background-color="#41b79d"
+						color="white"
+						border-radius="0"
+						padding="7px 5px"
+						align="center"
+						container-background-color="#41b79d"
+						font-size="14px"
+						font-weight="600"
+						font-family="Arial, Helvetica, sans-serif"
+						href={mjAttribute('href')}
+					>
+						<span style={{ textTransform: 'uppercase' }}>
+							{ content }
+						</span>
+					</MJMLButton>
+				</MJMLColumn>
+			</MJMLSection>
+		)
+	}
 }
 
 HeleButton.tagName = tagName;
